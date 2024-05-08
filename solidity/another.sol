e4be3544263017cae2e1634214f373877ecaaaf4
rohitrrr345 <rrajput2060@gmail.com>
2024-05-08 23:01:03 +0530

    comments got removed

--- solidity/another.sol
+++ solidity/another.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+ SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.8.2 <0.9.0;
 
@@ -12,43 +12,39 @@ struct student{
 
 
 contract array{
-    // bytes3  public b3;//3 bytes array
+ bytes3  public b3;//3 bytes array
 
-    // bytes2 public b2;//it stored in bytes
-    //  function setter() public{
-    //     b3='abc';
-    //     b2='ab';
-    //     // b3[0]='d';this cannot be done
-    //     // they are immutable
-    //  }this is lec 1
-    // bytes public b1="abc";
-    // function pushe() public{
-    //     b1.push('d');
-    // }
-    //   function getele(uint i) public view returns(bytes1){
-    //    return b1[i];
+ bytes2 public b2;//it stored in bytes
+  function setter() public{
+     b3='abc';
+     b2='ab';
+     // b3[0]='d';this cannot be done
+     // they are immutable
+  }this is lec 1
+ bytes public b1="abc";
+ function pushe() public{
+        b1.push('d');
+ }
+   function getele(uint i) public view returns(bytes1){
+    return b1[i];
 
     
-    // }
-    // function length() public view returns(uint){
-    //     return b1.length;
-    // }lec 2
-// uint[3] public arr;
-// uint public count;
-// // function loop() public{
-// // // while(count<arr.length){
-// // //     arr[count]=count;
-// // //     count++;
-// // // }
-// // // for(uint i=count;i<arr.length;i++){
-// // //     arr[count]=count;
-// // //     count++;
-// // // }
+ }
+ function length() public view returns(uint){
+     return b1.length;
+ }lec 2 uint[3] public arr; uint public count;
+// function loop() public{
+// // while(count<arr.length){
+// //     arr[count]=count;
+// //     count++;
+// // }
+// // for(uint i=count;i<arr.length;i++){
+// //     arr[count]=count;
+// //     count++;
+// // }
 
-// // }lec 3
-// function check(int a ) public pure returns(string memory){
-//     string memory value;
-//     if(a>0){
+// }lec 3
+// function check(int a ) public pure returns(string memory){     string memory value;     if(a>0){
 //         value="greater than 0";
 //     }
 //     else{
